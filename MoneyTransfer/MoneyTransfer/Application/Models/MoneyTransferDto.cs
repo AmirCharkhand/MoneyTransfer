37fb463f27039848f7228ff1e8c88576a53a744e
Amiroo <amir.charkhand@outlook.com>
2025-11-10 01:00:56 +0330

    DEVELOPMENT: money transfer endpoint development

---
+++ MoneyTransfer/MoneyTransfer/Application/Models/MoneyTransferDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoneyTransfer.Application.Models
+{
+    public class MoneyTransferDto
+    {
+        [Required]
+        public int FromAccountId { get; set; }
+
+        [Required]
+        public int ToAccountId { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public double Amount { get; set; }
+    }
+}
--- MoneyTransfer/MoneyTransfer/Controllers/AccountController.cs
+++ MoneyTransfer/MoneyTransfer/Controllers/AccountController.cs
@@ -1,19 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MoneyTransfer.Application.Models;
 using MoneyTransfer.Application.UseCases.Account;
 
 namespace MoneyTransfer.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AccountController(GetAccountBalanceUseCase getAccountBalanceUseCase) : ControllerBase
+    public class AccountController(GetAccountBalanceUseCase getAccountBalanceUseCase, TransferMoneyUseCase transferMoneyUseCase) : ControllerBase
     {
         private readonly GetAccountBalanceUseCase _getAccountBalanceUseCase = getAccountBalanceUseCase;
+        private readonly TransferMoneyUseCase _transferMoneyUseCase = transferMoneyUseCase;
 
         [HttpGet("balance/{id}")]
         public async Task<IActionResult> GetAccountBalance([FromRoute]int id)
         {
             var balance = await _getAccountBalanceUseCase.ExecuteAsync(id);
             return Ok(balance);
+        }
+
+        [HttpPost("transfer")]
+        public async Task<IActionResult> TransferMoney([FromBody]MoneyTransferDto moneyTransferDto)
+        {
+            await _transferMoneyUseCase.ExecuteAsync(moneyTransferDto.FromAccountId, moneyTransferDto.ToAccountId, moneyTransferDto.Amount);
+            return Ok();
         }
     }
 }
