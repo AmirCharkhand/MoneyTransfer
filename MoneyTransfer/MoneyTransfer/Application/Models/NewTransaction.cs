592a32dbcf68dfcc8cf6222c7d18ec09edc26315
Amiroo <amir.charkhand@outlook.com>
2025-11-10 00:50:30 +0330

    DEVELOPMENT: Transaction models development

---
+++ MoneyTransfer/MoneyTransfer/Application/Models/NewTransaction.cs
@@ -0,0 +1,42 @@
+﻿using MoneyTransfer.CoreBusiness.Enums;
+using MoneyTransfer.CoreBusiness.Models;
+
+namespace MoneyTransfer.Application.Models
+{
+    public class NewTransaction
+    {
+        public Guid Number { get; init; }
+        public int AccountId { get; init; }
+        public BankAcount Acount { get; init; }
+        public TransactionType Type { get; init; }
+        public double BalanceBefore { get; init; }
+        public double Amount { get; init; }
+        public double BalanceAfter { get; init; }
+        public DateTime TransactionTime { get; init; }
+
+        public NewTransaction(Guid number, int accountId, BankAcount acount, TransactionType type, double balanceBefore, double amount)
+        {
+            Number = number;
+            AccountId = accountId;
+            Acount = acount;
+            Type = type;
+            BalanceBefore = balanceBefore;
+            Amount = amount;
+            TransactionTime = DateTime.Now;
+            BalanceAfter = CalculateBalanceAfter();
+        }
+
+        private double CalculateBalanceAfter()
+        {
+            switch (this.Type)
+            {
+                case TransactionType.Incremental:
+                    return this.BalanceBefore + this.Amount;
+                case TransactionType.Decremental:
+                    return this.BalanceBefore - this.Amount;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(this.Type));
+            }
+        }
+    }
+}
--- MoneyTransfer/MoneyTransfer/CoreBusiness/Models/Transaction.cs
+++ MoneyTransfer/MoneyTransfer/CoreBusiness/Models/Transaction.cs
@@ -1,8 +1,17 @@
-﻿namespace MoneyTransfer.CoreBusiness.Models
+﻿using MoneyTransfer.CoreBusiness.Enums;
+
+namespace MoneyTransfer.CoreBusiness.Models
 {
     public class Transaction
     {
         public int ID { get; set; }
         public Guid Number { get; set; }
+        public int AccountId { get; set; }
+        public BankAcount? Acount { get; set; }
+        public TransactionType Type { get; set; }
+        public double BalanceBefore { get; set; }
+        public double Amount { get; set; }
+        public double BalanceAfter { get; set; }
+        public DateTime TransactionTime { get; set; }
     }
 }
