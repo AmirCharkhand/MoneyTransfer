3ff0406142ba55366cc817ea51b24c5076da1bed
Amiroo <amir.charkhand@outlook.com>
2025-11-09 17:06:09 +0330

    DEVELOPMENT: account controller development

---
+++ MoneyTransfer/MoneyTransfer/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoneyTransfer.Application.UseCases.Account;
+
+namespace MoneyTransfer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController(GetAccountBalanceUseCase getAccountBalanceUseCase) : ControllerBase
+    {
+        private readonly GetAccountBalanceUseCase _getAccountBalanceUseCase = getAccountBalanceUseCase;
+
+        [HttpGet("balance/{id}")]
+        public async Task<IActionResult> GetAccountBalance([FromRoute]int id)
+        {
+            var balance = await _getAccountBalanceUseCase.ExecuteAsync(id);
+            return Ok(balance);
+        }
+    }
+}
