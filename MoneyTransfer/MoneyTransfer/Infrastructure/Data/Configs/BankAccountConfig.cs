7b4699924a488d1d88308bd3fb14b5e3e3a68017
Amiroo <amir.charkhand@outlook.com>
2025-11-11 00:51:48 +0330

    DEVELOPMENT: EntityConfigurations for BanckAccount and Transaction Entities are developed

---
+++ MoneyTransfer/MoneyTransfer/Infrastructure/Data/Configs/BankAccountConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoneyTransfer.CoreBusiness.Models;
+
+namespace MoneyTransfer.Infrastructure.Data.Configs
+{
+    public class BankAccountConfig : IEntityTypeConfiguration<BankAcount>
+    {
+        public void Configure(EntityTypeBuilder<BankAcount> builder)
+        {
+            builder
+                .HasMany<Transaction>()
+                .WithOne(Transaction => Transaction.Acount)
+                .HasForeignKey(Transaction => Transaction.AccountId);
+
+            builder
+                .Property(ba => ba.Balance)
+                .HasAnnotation("RangeMin", 0)
+                .HasAnnotation("RangeMax", double.MaxValue);
+
+            builder
+                .HasData(
+                    new BankAcount { Id = 1, Balance = 10000000 },
+                    new BankAcount { Id = 2, Balance = 25000000 },
+                    new BankAcount { Id = 3, Balance = 5000000 }
+                );
+        }
+    }
+}
---
+++ MoneyTransfer/MoneyTransfer/Infrastructure/Data/Configs/TransactionConfig.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoneyTransfer.CoreBusiness.Models;
+
+namespace MoneyTransfer.Infrastructure.Data.Configs
+{
+    public class TransactionConfig : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder
+                .Property(t => t.Number)
+                .IsRequired();
+
+            builder
+                .Property(t => t.Amount)
+                .IsRequired()
+                .HasAnnotation("RangeMin", 0)
+                .HasAnnotation("RangeMax", double.MaxValue);
+
+            builder
+                .Property(t => t.TransactionTime)
+                .IsRequired();
+
+            builder
+                .Property(t => t.Type)
+                .IsRequired();
+
+            builder
+                .Property(t => t.BalanceBefore)
+                .IsRequired()
+                .HasAnnotation("RangeMin", 0)
+                .HasAnnotation("RangeMax", double.MaxValue);
+
+            builder
+                .Property(t => t.BalanceAfter)
+                .IsRequired()
+                .HasAnnotation("RangeMin", 0)
+                .HasAnnotation("RangeMax", double.MaxValue);
+        }
+    }
+}
