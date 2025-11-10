ee7526ff763ed3018f3f9abbaad12daaeb10a609
Amiroo <amir.charkhand@outlook.com>
2025-11-10 20:14:03 +0330

    DEVELOPMENT: Get last 10 transactions endpoint for the account controller is developed

--- MoneyTransfer/MoneyTransfer/Controllers/AccountController.cs
+++ MoneyTransfer/MoneyTransfer/Controllers/AccountController.cs
@@ -7,10 +7,14 @@ namespace MoneyTransfer.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AccountController(GetAccountBalanceUseCase getAccountBalanceUseCase, TransferMoneyUseCase transferMoneyUseCase) : ControllerBase
+    public class AccountController(
+        GetAccountBalanceUseCase getAccountBalanceUseCase, 
+        TransferMoneyUseCase transferMoneyUseCase,
+        Get10LastTransactionsForAccountUseCase get10LastTransactionsForAccountUseCase) : ControllerBase
     {
         private readonly GetAccountBalanceUseCase _getAccountBalanceUseCase = getAccountBalanceUseCase;
         private readonly TransferMoneyUseCase _transferMoneyUseCase = transferMoneyUseCase;
+        private readonly Get10LastTransactionsForAccountUseCase _get10LastTransactionsForAccountUseCase = get10LastTransactionsForAccountUseCase;
 
         [HttpGet("balance/{id}")]
         [Account_HandleAccountNotFoundExeption]
@@ -27,6 +31,14 @@ namespace MoneyTransfer.Controllers
         {
             await _transferMoneyUseCase.ExecuteAsync(moneyTransferDto.FromAccountId, moneyTransferDto.ToAccountId, moneyTransferDto.Amount);
             return Ok();
+        }
+
+        [HttpGet("last-10-transactions/{id}")]
+        [Account_HandleAccountNotFoundExeption]
+        public async Task<IActionResult> GetLast10Transactions([FromRoute] int id)
+        {
+            var transactions = await _get10LastTransactionsForAccountUseCase.ExecuteAsync(id);
+            return Ok(transactions);
         }
     }
 }
