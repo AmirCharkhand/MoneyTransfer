0995c9125b753fef79ebbb12a057aa4c6179797f
Amiroo <amir.charkhand@outlook.com>
2025-11-11 00:49:21 +0330

    DEVELOPMENT: The SQL Server DbContext is developed

--- MoneyTransfer/MoneyTransfer/Application/Extensions/AddApplicationServicesExtension.cs
+++ MoneyTransfer/MoneyTransfer/Application/Extensions/AddApplicationServicesExtension.cs
@@ -1,10 +1,16 @@
-﻿namespace MoneyTransfer.Application.Extensions
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyTransfer.Infrastructure.Data;
+
+namespace MoneyTransfer.Application.Extensions
 {
     public static class AddApplicationServicesExtension
     {
-        public static void AddApplicationServices(this IServiceCollection services)
+        public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddControllers();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DevelopmentConnectionString")));
 
             services
                 .AddRepositories()
---
+++ MoneyTransfer/MoneyTransfer/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyTransfer.CoreBusiness.Models;
+using System.Reflection;
+
+namespace MoneyTransfer.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BankAcount> BankAccounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
