40268e61702dc8b8354c5d1bc810678d40718f87
Amiroo <amir.charkhand@outlook.com>
2025-11-11 20:07:28 +0330

    REFACTORING: User model refactoring

--- MoneyTransfer/MoneyTransfer/CoreBusiness/Models/User.cs
+++ MoneyTransfer/MoneyTransfer/CoreBusiness/Models/User.cs
@@ -7,8 +7,8 @@
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string? PhoneNumber { get; set; }
-        public byte[]? PasswordHash { get; set; }
-        public byte[]? PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; } = Array.Empty<byte>();
+        public byte[] PasswordSalt { get; set; } = Array.Empty<byte>();
         public List<BankAcount>? Accounts { get; set; }
     }
 }
