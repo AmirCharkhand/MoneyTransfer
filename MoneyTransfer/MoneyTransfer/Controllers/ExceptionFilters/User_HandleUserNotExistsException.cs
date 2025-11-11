59b57f5ebb8c8a90b990649bfa40e4474aebdfc4
Amiroo <amir.charkhand@outlook.com>
2025-11-11 20:14:38 +0330

    DEVELOPMENT: User controller and user not exists exception filters are developed

---
+++ MoneyTransfer/MoneyTransfer/Controllers/ExceptionFilters/User_HandleUserNotExistsException.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MoneyTransfer.Application.Exceptions;
+
+namespace MoneyTransfer.Controllers.ExceptionFilters
+{
+    public class User_HandleUserNotExistsException : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            base.OnException(context);
+
+            if (context.Exception is UserNotExistsException)
+            {
+                context.ExceptionHandled = true;
+                context.HttpContext.Response.StatusCode = 404;
+                context.Result = new NotFoundObjectResult(context.Exception.Message);
+            }
+        }
+    }
+}
---
+++ MoneyTransfer/MoneyTransfer/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoneyTransfer.Application.Models;
+using MoneyTransfer.Application.UseCases.AuthenticationUseCases;
+using MoneyTransfer.Controllers.ExceptionFilters;
+
+namespace MoneyTransfer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController(LoginUseCase loginUseCase) : ControllerBase
+    {
+        private readonly LoginUseCase _loginUseCase = loginUseCase;
+
+        [HttpPost("login")]
+        [User_HandleUserNotExistsException]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto request)
+        {
+            var response = await _loginUseCase.ExecuteAsync(request.Email, request.Password);
+            return Ok(response);
+        }
+    }
+}
