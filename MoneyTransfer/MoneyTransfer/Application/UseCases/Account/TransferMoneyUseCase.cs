df541302f0346f9b21716b14efa5e13ac7b3f16b
Amiroo <amir.charkhand@outlook.com>
2025-11-11 10:59:26 +0330

    REFACTORING: Transfer money use case refactoring

--- MoneyTransfer/MoneyTransfer/Application/UseCases/Account/TransferMoneyUseCase.cs
+++ MoneyTransfer/MoneyTransfer/Application/UseCases/Account/TransferMoneyUseCase.cs
@@ -1,6 +1,7 @@
 ﻿using MoneyTransfer.Application.Exceptions;
 using MoneyTransfer.Application.Models;
 using MoneyTransfer.CoreBusiness.Enums;
+using MoneyTransfer.CoreBusiness.Models;
 using MoneyTransfer.Infrastructure.PluginContracts;
 
 namespace MoneyTransfer.Application.UseCases.Account
@@ -18,13 +19,36 @@ namespace MoneyTransfer.Application.UseCases.Account
             var toAccount = await _accountRepository.GetBankAcountByIdAsync(toAccountId)
                 ?? throw new AccountNotFoundException(toAccountId);
 
-            if (fromAccount.Balance < amount)
-                throw new NotEnoughAccountBalanceException();
+            CheckFromAccountBalance(amount, fromAccount);
 
+            await CheckDailyLimit(fromAccountId, amount);
+
+            await CreateTransactions(fromAccountId, toAccountId, amount, fromAccount, toAccount);
+
+            await UpdateBalances(fromAccountId, toAccountId, amount, fromAccount, toAccount);
+        }
+
+        private async Task CheckDailyLimit(int fromAccountId, double amount)
+        {
             var dailyTransfers = await _transactionRepository.GetDailyTransferAmountForAccount(fromAccountId, TransactionType.Decremental);
             if (dailyTransfers.Sum(t => t.Amount) + amount > 10000000)
                 throw new DailyTransferLimitReachedException();
+        }
 
+        private static void CheckFromAccountBalance(double amount, BankAcount fromAccount)
+        {
+            if (fromAccount.Balance < amount)
+                throw new NotEnoughAccountBalanceException();
+        }
+
+        private async Task UpdateBalances(int fromAccountId, int toAccountId, double amount, BankAcount fromAccount, BankAcount toAccount)
+        {
+            await _accountRepository.UpdateAccountBalance(fromAccountId, fromAccount.Balance - amount);
+            await _accountRepository.UpdateAccountBalance(toAccountId, toAccount.Balance + amount);
+        }
+
+        private async Task CreateTransactions(int fromAccountId, int toAccountId, double amount, BankAcount fromAccount, BankAcount toAccount)
+        {
             var fromAccountTransaction = new NewTransaction(
                 Guid.NewGuid(),
                 fromAccountId,
@@ -43,9 +67,6 @@ namespace MoneyTransfer.Application.UseCases.Account
 
             await _transactionRepository.AddTransaction(fromAccountTransaction);
             await _transactionRepository.AddTransaction(toAccountTransaction);
-
-            await _accountRepository.UpdateAccountBalance(fromAccountId, fromAccount.Balance - amount);
-            await _accountRepository.UpdateAccountBalance(toAccountId, toAccount.Balance + amount);
         }
     }
 }
