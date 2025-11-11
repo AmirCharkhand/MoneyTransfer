57256b6bbf18f9baabf912f11da13b008308ea7e
Amiroo <amir.charkhand@outlook.com>
2025-11-11 20:06:27 +0330

    DEVELOPMENT: Verify hash method is added to Hash service

--- MoneyTransfer/MoneyTransfer/Application/Services/HashService.cs
+++ MoneyTransfer/MoneyTransfer/Application/Services/HashService.cs
@@ -10,5 +10,12 @@ namespace MoneyTransfer.Application.Services
             salt = hmac.Key;
             textHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(text));
         }
+
+        public bool VerifyHash(string text, byte[] hashedText, byte[] salt)
+        {
+            var hmac = new HMACSHA512(salt);
+            var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(text));
+            return computeHash.SequenceEqual(hashedText);
+        }
     }
 }
